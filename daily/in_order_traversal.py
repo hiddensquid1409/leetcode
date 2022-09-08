4c1479f3f6783f0564b8425ebebae55100f764f9
Jorge Garcia Serna <jorge.garcias@softtek.com>
2022-09-08 15:53:20 -0500

    added daily 9/8

---
+++ daily/in_order_traversal.py
@@ -0,0 +1,36 @@
+# Definition for a binary tree node.
+"""
+https://leetcode.com/problems/binary-tree-inorder-traversal/
+
+94. Binary Tree Inorder Traversal
+
+Given the root of a binary tree, return the inorder traversal of its nodes' values.
+
+ 
+"""
+
+
+class TreeNode:
+    def __init__(self, val=0, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
+
+
+class Solution:
+    def inorderTraversal(self, root: Optional[TreeNode]) -> List[int]:
+        if not root:
+            return []
+
+        def inOrder(node, output):
+            if not node:
+                return
+
+            if node.left:
+                inOrder(node.left, output)
+            output.append(node.val)
+            if node.right:
+                inOrder(node.right, output)
+            return output
+
+        return inOrder(root, [])
